ee5c3e8cd7dd4fdad7124b6e161771bdcd6a56d9
Jefferson Wu <jefferson.wu@180la.com>
2016-05-10 19:15:49 -0700

    dependencies set up, ready to module-ise

--- app.js
+++ app.js
@@ -2,3 +2,12 @@
  * 180 Faces - Hardware Module
  * Created by jefferson.wu on 5/9/16.
  */
+
+module.exports = {
+    saySomething: function(message){
+        console.log('my message: ' + message);
+    },
+    vomitSomething: function(vmessage){
+        console.log('blarggg!: ' + vmessage);
+    }
+};
